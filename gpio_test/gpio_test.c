02c837e340d275e157a5afc5af24aa76ad745cf6
Michael.Ma <guo.liang.ma@autodesk.com>
2025-09-09 13:44:06 +0800

    gpio_test

---
+++ gpio_test/gpio_test.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#include <wiringx.h>
+
+int main() {
+    int DUO_GPIO = 15;
+
+    // Duo:     milkv_duo
+    // Duo256M: milkv_duo256m
+    // DuoS:    milkv_duos
+    if(wiringXSetup("milkv_duo", NULL) == -1) {
+        wiringXGC();
+        return -1;
+    }
+
+    if(wiringXValidGPIO(DUO_GPIO) != 0) {
+        printf("Invalid GPIO %d\n", DUO_GPIO);
+    }
+
+    pinMode(DUO_GPIO, PINMODE_OUTPUT);
+
+    while(1) {
+        printf("Duo GPIO (wiringX) %d: High\n", DUO_GPIO);
+        digitalWrite(DUO_GPIO, HIGH);
+        sleep(1);
+        printf("Duo GPIO (wiringX) %d: Low\n", DUO_GPIO);
+        digitalWrite(DUO_GPIO, LOW);
+        sleep(1);
+    }
+
+    return 0;
+}
